1fdd78aafaba6c32c90040e596994c4f4c09cb38
unknown <fulltopic@163.com>
2022-11-21 16:25:33 +0800

    sqlite and lzma libs

--- scripts/env_cross/buildconfigs/ios_toolchain.cmake
+++ scripts/env_cross/buildconfigs/ios_toolchain.cmake
@@ -7,3 +7,11 @@ set(CMAKE_CXX_COMPILER ${IOS_TOOLCHAIN_BIN_PATH}/arm-apple-darwin21-clang++)
 set(CMAKE_AR ${IOS_TOOLCHAIN_BIN_PATH}/arm-apple-darwin21-ar)
 set(CMAKE_RANLIB ${IOS_TOOLCHAIN_BIN_PATH}/arm-apple-darwin21-ranlib)
 
+# sqlite: nohave_system, iphone_simulator
+# TODO: special flags relayed to specific project
+set(IOS_CFLAGS "-DSQLITE_NOHAVE_SYSTEM ")
+set(IOS_CXXFLAGS "-DSQLITE_NOHAVE_SYSTEM ")
+
+set(TARGET_EXTRA_CFLAGS ${IOS_CFLAGS})
+set(TARGET_EXTRA_CXXFLAGS ${IOS_CXXFLAGS})
+
