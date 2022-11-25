e16c8c9d71192c7a5bf2eb9361019306ac6f3dd2
unknown <fulltopic@163.com>
2022-11-25 08:18:54 +0800

    more opencv

---
+++ scripts/env_cross/opencv/cmake_user/Common.cmake
@@ -0,0 +1,49 @@
+# build target
+set(CMAKE_CROSSCOMPILING 1)
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build")
+  #set(CMAKE_BUILD_TYPE CACHE STRING"Release")
+
+# other options
+set(ENABLE_PIC 1)
+set(BUILD_SHARED_LIBS 0)
+
+set(BUILD_DOCS 0)
+set(BUILD_opencv_js 0)
+set(BUILD_PERF_TEST 0)
+set(BUILD_FAT_JAVA_LIB 0)
+set(BUILD_ANDROID_SERVICE 0)
+set(BUILD_JAVA 0)
+set(BUILD_OBJC 0)
+set(BUILD_KOTLIN_EXTENSIONS 0)
+
+set(INSTALL_PYTHON_EXAMPLES 0)
+set(INSTALL_ANDROID_EXAMPLES 0)
+set(INSTALL_TO_MANGLED_PATHS 0)
+
+set(CV_TRACE 1)
+
+set(OPENCV_README_FILE 0)
+set(OPENCV_LICENSE_FILE 0)
+
+set(OPENCV_FORCE_3RDPARTY_BUILD 1)
+set(BUILD_PERF_TESTS 0)
+set(BUILD_TESTS 0)
+set(BUILD_WITH_DYNAMIC_IPP 0)
+
+set(ENV{OPENCV_IPP_PATH} /home/user1/workspaces/opencv-4.x/build/3rdparty/ippicv/ippicv_mac)
+
+
+# CMakeLists.txt
+set(WORDS_BIGENDIAN 0)
+# libtiff
+set(bigendian 0)
+# openjpeg
+set(OPJ_BIG_ENDIAN 0)
+
+#tmp
+set(ANDROID 1)
+set(BUILD_ANDROID_EXAMPLES 0)
+set(INSTALL_ANDROID_EXAMPLES 0)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++11")
+#set(OPENCV_EXTRA_FLAGS "${OPENCV_EXTRA_FLAGS_DEBUG} -stdlib=libstdc++")
+
